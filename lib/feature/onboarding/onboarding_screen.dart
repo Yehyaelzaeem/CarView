57f3c709834ffaf46270d9e5655df56fa3f088a7
Yahia <Yehyaelzaeem>
2023-12-07 15:47:49 +0000

    fack

--- lib/feature/onboarding/onboarding_screen.dart
+++ lib/feature/onboarding/onboarding_screen.dart
@@ -16,6 +16,7 @@ class OnBoardingScreen extends StatelessWidget {
           padding: EdgeInsets.only(top: 70.h,bottom: 30.h),
           child:  Column(
             children: [
+
               const CarViewLogoWidget(),
               SizedBox(height: 50.h,),
               const CarImageOnBoarding(),
